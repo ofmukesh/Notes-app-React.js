8e980ee47f03a9e35f51e6cb8f90346c6351d399
x1mukesh <of.mukesh@gmail.com>
2023-08-20 10:25:53 +0530

    add btn init

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,19 +2,35 @@ import React from "react";
 import SearchBar from "../components/search_bar";
 import NotesCard from "../components/notes_card";
 import "../assets/styles/notes.css";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 
 const Home = () => {
   const notes = useSelector((state) => state.notes.notes);
 
   return (
     <>
-      <h2 className="mt-3">
-        <b>Notes</b>
+      <h2 className="notes_container mt-3">
+        {/* notes header */}
+        <div className="row notes_header">
+          {/* notes title */}
+          <div className="col">
+            <b>Notes</b>
+          </div>
+          {/* add notes */}
+          <div className="col text-end">
+            <button className="add_btn" type="button">
+              <i className="fa-solid fa-plus"></i>
+            </button>
+          </div>
+        </div>
       </h2>
+
+      {/* search notes */}
       <div className="mt-3">
         <SearchBar />
       </div>
+
+      {/* notes */}
       <div className="container">
         <div className="mt-2 row row-cols-1 row-cols-lg-3 row-cols-md-2">
           {notes &&
